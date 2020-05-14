0d55299d662d2f6aa91746b24190c62289376098
phbi <phbi@itu.dk>
2020-05-14 20:53:18 +0200

    api fix

--- src/js/api.js
+++ src/js/api.js
@@ -6,11 +6,17 @@ async function changeTheme(clicked_id) {
     document.getElementById("nav").classList.add('indigo');
     document.getElementById("upload_btn").classList.add('blue');
     document.getElementById("title").style["font-family"] = "Helvetica";
-    document.getElementById("pic-title").style["font-family"] = "Helvetica";
+    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
+        document.getElementsByClassName("card-title")[i].style["font-name"] = "Helvetica";
+        document.getElementsByClassName("card-title")[i].style["font-family"] = "Helvetica";
+    }
+    console.log(document.getElementsByClassName("card-title"));
   }
   if(clicked_id == "1") {
-    document.getElementById("pic-title").style["font-name"] = json.themes[0].styles['fontName'];
-    document.getElementById("pic-title").style["font-family"] = json.themes[0].styles['fontFamily'];
+    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
+        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[0].styles['fontName'];
+        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[0].styles['fontFamily'];
+    }
     document.getElementById("title").style["font-family"] = json.themes[0].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[0].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[0].styles['secondaryColor'];
@@ -18,8 +24,10 @@ async function changeTheme(clicked_id) {
     document.getElementById("upload_btn").classList.remove('blue');
   }
   if(clicked_id == "2") {
-    document.getElementById("pic-title").style["font-name"] = json.themes[1].styles['fontName'];
-    document.getElementById("pic-title").style["font-family"] = json.themes[1].styles['fontFamily'];
+    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
+        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[1].styles['fontName'];
+        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[1].styles['fontFamily'];
+    }
     document.getElementById("title").style["font-family"] = json.themes[1].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[1].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[1].styles['secondaryColor'];
@@ -27,8 +35,10 @@ async function changeTheme(clicked_id) {
     document.getElementById("upload_btn").classList.remove('blue');
   }
   if(clicked_id == "3") {
-    document.getElementById("pic-title").style["font-name"] = json.themes[2].styles['fontName'];
-    document.getElementById("pic-title").style["font-family"] = json.themes[2].styles['fontFamily'];
+    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
+        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[2].styles['fontName'];
+        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[2].styles['fontFamily'];
+    }
     document.getElementById("title").style["font-family"] = json.themes[2].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[2].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[2].styles['secondaryColor'];
