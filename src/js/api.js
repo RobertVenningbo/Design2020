bcad2bd3c912fef3d6076934bcf373df965df554
phbi <phbi@itu.dk>
2020-05-14 18:33:31 +0200

    api fix

---
+++ src/js/api.js
@@ -0,0 +1,42 @@
+const api = 'https://itu-sdbg-s2020.now.sh/api/themes';
+async function changeTheme(clicked_id) {
+  const response = await fetch(api);
+  const json = await response.json();
+  if(clicked_id == "0") {
+    document.getElementById("nav").classList.add('indigo');
+    document.getElementById("upload_btn").classList.add('blue');
+    document.getElementById("title").style["font-family"] = "Helvetica";
+    document.getElementById("card-title").style["font-family"] = "Helvetica";
+  }
+  if(clicked_id == "1") {
+    console.log(json.themes[0].id);
+    console.log(json.themes[0].styles['secondaryColor']);
+    document.getElementById("card-title").style["font-name"] = json.themes[0].styles['fontName'];
+    document.getElementById("card-title").style["font-family"] = json.themes[0].styles['fontFamily'];
+    document.getElementById("title").style["font-family"] = json.themes[0].styles['fontFamily'];
+    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[0].styles['primaryColor'];
+    document.getElementById("nav").style.backgroundColor = "#" + json.themes[0].styles['secondaryColor'];
+    document.getElementById("nav").classList.remove('indigo');
+    document.getElementById("upload_btn").classList.remove('blue');
+  }
+  if(clicked_id == "2") {
+    console.log(json.themes[1].id);
+    document.getElementById("card-title").style["font-name"] = json.themes[1].styles['fontName'];
+    document.getElementById("card-title").style["font-family"] = json.themes[1].styles['fontFamily'];
+    document.getElementById("title").style["font-family"] = json.themes[1].styles['fontFamily'];
+    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[1].styles['primaryColor'];
+    document.getElementById("nav").style.backgroundColor = "#" + json.themes[1].styles['secondaryColor'];
+    document.getElementById("nav").classList.remove('indigo');
+    document.getElementById("upload_btn").classList.remove('blue');
+  }
+  if(clicked_id == "3") {
+    console.log(json.themes[2].id);
+    document.getElementById("card-title").style["font-name"] = json.themes[2].styles['fontName'];
+    document.getElementById("card-title").style["font-family"] = json.themes[2].styles['fontFamily'];
+    document.getElementById("title").style["font-family"] = json.themes[2].styles['fontFamily'];
+    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[2].styles['primaryColor'];
+    document.getElementById("nav").style.backgroundColor = "#" + json.themes[2].styles['secondaryColor'];
+    document.getElementById("nav").classList.remove('indigo');
+    document.getElementById("upload_btn").classList.remove('blue');
+  }
+}
--- src/js/javascript.js
+++ src/js/javascript.js
@@ -4,12 +4,12 @@
 */
 var i = 0;
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems);
 });
-/*
+
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
   var instances = M.Sidenav.init(elems);
@@ -133,50 +133,4 @@ textarea.onkeyup = function() {
 };
  ev.currentTarget.replaceChild (span, spanTgt);
  spanParent.appendChild (spanTgt);
-})
 
-const api = 'https://itu-sdbg-s2020.now.sh/api/themes';
-async function changeTheme(clicked_id) {
-  const response = await fetch(api);
-  const json = await response.json();
-  if(clicked_id == "0") {
-    document.getElementById("nav").classList.add('indigo');
-    document.getElementById("upload_btn").classList.add('blue');
-    document.getElementById("title").style["font-family"] = "Helvetica";
-    document.getElementById("card-title").style["font-family"] = "Helvetica";
-
-
-    
-  }
-  if(clicked_id == "1") {
-    console.log(json.themes[0].id);
-    console.log(json.themes[0].styles['secondaryColor']);
-    document.getElementById("card-title").style["font-name"] = json.themes[0].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[0].styles['fontFamily'];
-    document.getElementById("title").style["font-family"] = json.themes[0].styles['fontFamily'];
-    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[0].styles['primaryColor'];
-    document.getElementById("nav").style.backgroundColor = "#" + json.themes[0].styles['secondaryColor'];
-    document.getElementById("nav").classList.remove('indigo');
-    document.getElementById("upload_btn").classList.remove('blue');
-  }
-  if(clicked_id == "2") {
-    console.log(json.themes[1].id);
-    document.getElementById("card-title").style["font-name"] = json.themes[1].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[1].styles['fontFamily'];
-    document.getElementById("title").style["font-family"] = json.themes[1].styles['fontFamily'];
-    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[1].styles['primaryColor'];
-    document.getElementById("nav").style.backgroundColor = "#" + json.themes[1].styles['secondaryColor'];
-    document.getElementById("nav").classList.remove('indigo');
-    document.getElementById("upload_btn").classList.remove('blue');
-  }
-  if(clicked_id == "3") {
-    console.log(json.themes[2].id);
-    document.getElementById("card-title").style["font-name"] = json.themes[2].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[2].styles['fontFamily'];
-    document.getElementById("title").style["font-family"] = json.themes[2].styles['fontFamily'];
-    document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[2].styles['primaryColor'];
-    document.getElementById("nav").style.backgroundColor = "#" + json.themes[2].styles['secondaryColor'];
-    document.getElementById("nav").classList.remove('indigo');
-    document.getElementById("upload_btn").classList.remove('blue');
-  }
-}
