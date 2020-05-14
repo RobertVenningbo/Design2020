68827777acde6a74f8384783b47ed54973a95588
phbi <phbi@itu.dk>
2020-05-14 20:08:21 +0200

    id fix

--- src/js/api.js
+++ src/js/api.js
@@ -11,8 +11,8 @@ async function changeTheme(clicked_id) {
   if(clicked_id == "1") {
     console.log(json.themes[0].id);
     console.log(json.themes[0].styles['secondaryColor']);
-    document.getElementById("card-title").style["font-name"] = json.themes[0].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[0].styles['fontFamily'];
+    document.getElementById("pic-title").style["font-name"] = json.themes[0].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[0].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[0].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[0].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[0].styles['secondaryColor'];
@@ -21,8 +21,8 @@ async function changeTheme(clicked_id) {
   }
   if(clicked_id == "2") {
     console.log(json.themes[1].id);
-    document.getElementById("card-title").style["font-name"] = json.themes[1].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[1].styles['fontFamily'];
+    document.getElementById("pic-title").style["font-name"] = json.themes[1].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[1].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[1].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[1].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[1].styles['secondaryColor'];
@@ -31,8 +31,8 @@ async function changeTheme(clicked_id) {
   }
   if(clicked_id == "3") {
     console.log(json.themes[2].id);
-    document.getElementById("card-title").style["font-name"] = json.themes[2].styles['fontName'];
-    document.getElementById("card-title").style["font-family"] = json.themes[2].styles['fontFamily'];
+    document.getElementById("pic-title").style["font-name"] = json.themes[2].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[2].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[2].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[2].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[2].styles['secondaryColor'];
