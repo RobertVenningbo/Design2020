61a2b4d3f63155d52a3de569f88e54d2febd84f9
Oskar Jensen <osje@itu.dk>
2020-05-15 11:56:01 +0200

    Revert "Merge branch 'master' into CanvaChanges"
    This reverts commit e95817aa1c46529b87982f035e38270429ee65aa, reversing
    changes made to 839c3fe7a59dc02e6fe5a80832487127b802d2d4.

--- src/js/api.js
+++ src/js/api.js
@@ -6,16 +6,11 @@ async function changeTheme(clicked_id) {
     document.getElementById("nav").classList.add('indigo');
     document.getElementById("upload_btn").classList.add('blue');
     document.getElementById("title").style["font-family"] = "Helvetica";
-    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
-        document.getElementsByClassName("card-title")[i].style["font-name"] = "Helvetica";
-        document.getElementsByClassName("card-title")[i].style["font-family"] = "Helvetica";
-    }
+    document.getElementById("pic-title").style["font-family"] = "Helvetica";
   }
   if(clicked_id == "1") {
-    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
-        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[0].styles['fontName'];
-        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[0].styles['fontFamily'];
-    }
+    document.getElementById("pic-title").style["font-name"] = json.themes[0].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[0].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[0].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[0].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[0].styles['secondaryColor'];
@@ -23,10 +18,8 @@ async function changeTheme(clicked_id) {
     document.getElementById("upload_btn").classList.remove('blue');
   }
   if(clicked_id == "2") {
-    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
-        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[1].styles['fontName'];
-        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[1].styles['fontFamily'];
-    }
+    document.getElementById("pic-title").style["font-name"] = json.themes[1].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[1].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[1].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[1].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[1].styles['secondaryColor'];
@@ -34,10 +27,8 @@ async function changeTheme(clicked_id) {
     document.getElementById("upload_btn").classList.remove('blue');
   }
   if(clicked_id == "3") {
-    for(var i = 0; i < document.getElementsByClassName("card-title").length; i++) {
-        document.getElementsByClassName("card-title")[i].style["font-name"] = json.themes[2].styles['fontName'];
-        document.getElementsByClassName("card-title")[i].style["font-family"] = json.themes[2].styles['fontFamily'];
-    }
+    document.getElementById("pic-title").style["font-name"] = json.themes[2].styles['fontName'];
+    document.getElementById("pic-title").style["font-family"] = json.themes[2].styles['fontFamily'];
     document.getElementById("title").style["font-family"] = json.themes[2].styles['fontFamily'];
     document.getElementById("upload_btn").style.backgroundColor = "#" + json.themes[2].styles['primaryColor'];
     document.getElementById("nav").style.backgroundColor = "#" + json.themes[2].styles['secondaryColor'];
